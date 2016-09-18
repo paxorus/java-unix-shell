8072b58a28f05ca20b2a6512de1974fd790aafb3
paxorus <prakhar@brandeis.edu>
2016-09-17 23:08:35 -0400

    Switching cd path correction methods
    Compounding file pathway in this commit (eliminating /.. and /.
    occurrences) but logic grew too complicated and not checking file
    existence at each step.

--- src/cs131/pa1/filter/sequential/CdFilter.java
+++ src/cs131/pa1/filter/sequential/CdFilter.java
@@ -23,11 +23,17 @@ public class CdFilter extends SequentialFilter {
 		path = params[1];
 		
 		// correct path
-		path = path.replaceAll("[\\/]", FILE_SEPARATOR);
+		if (FILE_SEPARATOR.equals("/")) {
+			path = path.replaceAll("\\\\", "/");
+		} else if (FILE_SEPARATOR.equals("\\")) {
+			path = path.replaceAll("/", "\\\\");
+		}
 		if (!path.startsWith(FILE_SEPARATOR)) {
+			// "poo/" --> "/poo/"
 			path = FILE_SEPARATOR + path;
 		}
 		if (path.endsWith(FILE_SEPARATOR)) {
+			// "/poo/" --> "/poo"
 			path = path.substring(0, path.length() - 1);
 		}
 	}
@@ -47,13 +53,41 @@ public class CdFilter extends SequentialFilter {
 			return;
 		}
 		
-		pwd += path;
-		
+		pwd = compact(pwd + path);
 		if (!(new File(pwd)).isDirectory()) {
 			throw new RuntimeException(Message.DIRECTORY_NOT_FOUND.with_parameter("cd"));
 		}
 		
 		SequentialREPL.currentWorkingDirectory = pwd;
+	}
+	
+	private String compact(String path) {
+
+		// eliminate all "/."
+		path = path.replaceAll(getSep() + "\\." + getSep(), getSep());
+		if (path.endsWith(FILE_SEPARATOR + ".")) {
+			System.out.println("here");
+			path = path.substring(0, path.length() - FILE_SEPARATOR.length() - 1);
+		}
+		
+		// eliminate all "/x/.."
+		int idx = path.indexOf("..");
+		while (idx != -1) {
+			// path is ".../x/../..."
+			String inspect = path.substring(0, idx - FILE_SEPARATOR.length());// ".../x"
+			int endIdx = inspect.lastIndexOf(FILE_SEPARATOR);
+			path = path.substring(0, endIdx) + path.substring(idx + 2);// "..." + "/..."
+			idx = path.indexOf("..");
+		}
+		
+
+		
+		return path;
+	}
+	
+	private String getSep() {
+		// regex form of the separator, introduces double-escape
+		return (FILE_SEPARATOR.equals("\\")? "\\\\" : "/");
 	}
 	
 	@Override
